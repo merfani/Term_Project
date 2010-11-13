fe0ecccd782a9e35dd5bf03c9e4e3eb96b2fd441
saturn <saturn@ubuntu.(none)>
2010-11-13 07:48:30 -0800

    Added next prev functionality to browse

--- browsesystem.php
+++ browsesystem.php
@@ -2,6 +2,29 @@
 include("include/connectdb.inc");
 include("include/utils.inc");
 
+function isSelected($array, $item)
+{
+
+    foreach ($array as $val){
+        if($val == $item) return "SELECTED";
+    }
+    return "";
+
+}
+
+function removeNotSpecified(&$array){
+    
+    foreach ($array as $i => $value) {
+        if($value == "Not Specified"){
+            unset($array[$i]);
+            return;
+        }
+    }    
+       
+        
+}
+
+
 
 $title = "Browse System";
 $javascript = "include/addsystem.js";
@@ -10,6 +33,7 @@ $types = "";
 
 if (isset($_REQUEST["server_type"])){
     $type = $_REQUEST["server_type"];
+    removeNotSpecified($type);
     $num_types = count($type); 
 }
 else{
@@ -17,10 +41,11 @@ else{
     $num_types = 0;
 }
 
-
+$t1=$type[0];
 
 if (isset($_REQUEST["os_type"])){
     $os = $_REQUEST["os_type"];
+    removeNotSpecified($os);
     $num_os = count($os);
 }
 else{
@@ -36,14 +61,21 @@ else{
 }
 
 
-$display_index=0;
+if (isset($_REQUEST["start_index"])){
+    $display_index = $_REQUEST["start_index"];
+}else{
+
+    $display_index=0;
+}
+
+
 include("include/header.inc");
 ?>
 
 <table id="mainTable" class="browseTableStyle">
 <tr><td>
 
-<form method="post" action="browsesystem.php">
+<form method="post" id="myform" name="myform" action="browsesystem.php">
 <table id="displayTable" class="formtable">
 <tr>
     <td colspan="2"><div class="formtitle">Browse Systems</div></td>
@@ -53,17 +85,17 @@ include("include/header.inc");
     <div>Types</div>    
     <?php
                 $types = gettypes();
+                echo "<select name=\"server_type[]\" size=\"5\" MULTIPLE>\n";
                 
-               # echo "<div style=\"height: 100px width: 200px; overflow: scroll;\">";
-               # foreach($types as $t){
-                #    echo "<input id=\"$t\" type=\"checkbox\" name=\"server_type[]\"/>";
-                #    echo "<label for=\"$t\">$t</label><br>";
-               # }
+                if($num_types==0){
+                    echo "<option \"SELECTED\">Not Specified</option>";
+                }else{
+                    echo "<option>Not Specified</option>";
+                }
 
-                 echo "<select name=\"server_type[]\" size=\"5\" MULTIPLE>\n";
-                echo "<option SELECTED>Not Specified</option>\n";
-                foreach ($types as $t) {
-                    echo "<option>$t</option>\n";
+                  foreach ($types as $t) {
+                      $select = isSelected($type, $t);
+                     echo "<option $select>$t</option>\n";
                 }
                 echo "</select>\n";
 
@@ -73,11 +105,18 @@ include("include/header.inc");
      <td id="ostd">
             <div>OS</div>    
             <?php
-                $oses = getoses();
+                $oses = getos();
                 echo "<select name=\"os_type[]\" size=\"5\" multiple=\"yes\">\n";
-                echo "<option SELECTED>Not Specified</option>\n";
+                if($num_os==0){
+                    echo "<option \"SELECTED\">Not Specified</option>";
+                }else{
+                    echo "<option>Not Specified</option>";
+                }
+
+
                 foreach ($oses as $t) {
-                    echo "<option>$t</option>\n";
+                    $select = isSelected($os, $t);
+                    echo "<option $select>$t</option>\n";
                 }
                 echo "</select>\n";
            ?>
@@ -90,6 +129,7 @@ include("include/header.inc");
     
     </td> 
     <td>
+        <input type="hidden" name="start_index" value="0">
         <input type="submit" value="search">
     </td>
 </tr>
@@ -97,14 +137,27 @@ include("include/header.inc");
 </form>
 </td></tr> 
 
+<script type="text/javascript">
+    function submitform(newStart){
+        document.myform["start_index"].value = newStart;
+        document.myform.submit();
+    }
+</script>
+
 <?php
 
-if($num_types != 0){
-   
-    
+    if($num_types != 0){
+        $type_mysql = "systems.type='" . implode("' or systems.type='", $type) . "'";
+    }else{
+        $type_mysql = "systems.type IS NOT NULL";
+    }  
 
-   $type_mysql = "systems.type='" . implode("' or systems.type='", $type) . "'";
- 
+    if($num_os != 0){
+        $type_os = "operating_systems.name='" . implode("' or operating_systems.name='", $os) . "'"; 
+    }else{
+        $type_os = "operating_systems.name IS NOT NULL";
+    }
+
     $order_field = "Hostname";
     $order_direction = "asc";
     $upper_limit= $display_index + $display_limit;
@@ -114,10 +167,10 @@ if($num_types != 0){
                                          operating_systems.name as OS,
                                          operating_systems.version as Version
                  from systems JOIN operating_systems ON (systems.os_id=operating_systems.id)
-                        where $type_mysql order by $order_field $order_direction
+                        where (($type_mysql) AND ($type_os)) order by $order_field $order_direction
          limit $display_index, $upper_limit";
 
-    echo "QUERY: $query";
+   # echo "QUERY: $query";
 
     $result = mysql_query($query);
 
@@ -129,20 +182,44 @@ if($num_types != 0){
 
     $start_index=$display_index+1;
     $end_index=$display_index+$num_rows;
+    
+    if($num_rows > 0){
+    echo "<tr><td>";
+    
+   echo "<table width=\"100%\"><tr><style type=\"text/css\"> td.jump {text-align: right}</style>";
+      
+     echo "<td>Displaying $start_index-$end_index from a total of  $total_num_rows results</td>";
+    
+     echo "<td class=\"jump\">";
+        
+    if($display_index != 0){
+        $newIndex = $display_index - $display_limit;
+        echo "<a href=\"javascript: submitform($newIndex)\">Prev</a>"; 
+        if($end_index < $total_num_rows){echo "|";}
+    }
+   
+    if($end_index < $total_num_rows){
+        echo "<a href=\"javascript: submitform($end_index)\"> Next</a>";
+    }
+    echo "</td>";
+    echo "</tr></table>";
 
-   echo "<tr><td>";
-    echo "<table id=\"results\" cellpadding=\"3\" cellspacing=\"0\" border=\"1\" class=\"resultstyle\">";
-    echo "Displaying $start_index-$end_index from a total of  $total_num_rows results";
- 
+
+    echo "</tr></td>";
+    echo "<tr><td>";
+     echo "<table id=\"results\" cellpadding=\"3\" cellspacing=\"0\" border=\"1\" class=\"resultstyle\">";
     
  
     echo "<tr id=\"resultFields\">";
     while( $property = mysql_fetch_field($result) ){
         echo "<td><b>$property->name</b></td>";
     }
-    echo "<td />"; 
+    echo "<td />"; #fill in corner square 
     echo "</tr>";
-     while($row = mysql_fetch_array($result)){
+
+
+
+    while($row = mysql_fetch_array($result)){
         echo "<tr>";
         $hostname       =   $row['Hostname'];        
         $description    =   $row['Description'];
@@ -154,16 +231,18 @@ if($num_types != 0){
         echo "<td>$type</td>";
         echo "<td>$os_id</td>";
         echo "<td>$version</td>";
-        echo "<td><a href=\"\">+</a></td></tr>";
+        echo "<td><a href=\"\">+</a></td>";
+        echo "</tr>";
 
 
     }
    
-     echo "</td></tr></table></tr>";
+     echo "</table>";
+     echo "</td></tr>";
 
-   
+   }
     mysql_free_result($result);
-}
+
 
 ?>
 </table>
